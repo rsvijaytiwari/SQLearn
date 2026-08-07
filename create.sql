91523686de86402a447b27ab89e9907a6d5e0b46
bhakti <dev@luxecomforttravels.in>
2026-08-07 19:23:10 +0530

    Add SQL scripts for employee, teacher, and student tables with sample data and queries

--- create.sql
+++ create.sql
@@ -32,4 +32,46 @@ CREATE TABLE orders (
     customer_id INT NOT NULL ,
     item VARCHAR(55) NOT NULL,
     CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers(id)
-)
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    salary DECIMAL (10,2) DEFAULT 0 CHECK ( salary > 0 )
+
+);
+
+INSERT INTO employee VALUE (2 , 150000);
+
+SELECT * FROM employee;
+
+
+CREATE TABLE teacher (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(55) DEFAULT 'Bhakti'
+);
+
+CREATE TABLE student (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(55) NOT NULL,
+    teacher_id INT,
+    CONSTRAINT fk_teacher_id FOREIGN KEY (teacher_id) REFERENCES teacher(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO teacher VALUE (1 , 'Vijay'),
+(2 , 'Ishwari');
+
+INSERT INTO student VALUE (1 , 'Vijay' , 1),
+(2 , 'Radhe' , 2);
+
+SELECT * FROM teacher JOIN student s on teacher.id = s.teacher_id;
+
+UPDATE teacher set id = 5 WHERE id = 1;
+
+DELETE FROM teacher WHERE id = 5;
+
+CREATE TABLE teacher2 (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(55)
+)AS SELECT id , name FROM teacher;
+
+SELECT * FROM teacher2;
