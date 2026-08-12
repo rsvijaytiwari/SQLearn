12a1bb889ceb1b90f82a25ec64624bf92a8d72f9
Bhakti-Panchal-13 <bhaktipanchal13g@gmail.com>
2026-08-12 20:24:33 +0530

    Add SQL scripts for employee, customer, and orders tables with queries and data manipulation

--- create.sql
+++ create.sql
@@ -44,6 +44,31 @@ INSERT INTO employee VALUE (2 , 150000);
 
 SELECT * FROM employee;
 
+SELECT salary FROM employee WHERE salary > 50000;
+
+ALTER TABLE employee ADD COLUMN name VARCHAR(55);
+ALTER TABLE employee CHANGE COLUMN department department VARCHAR(55);
+ALTER TABLE employee DROP CONSTRAINT department;
+ALTER TABLE employee DROP INDEX department;
+SHOW INDEX FROM employee WHERE Non_unique = 0;
+ALTER TABLE employee DROP INDEX department_2;
+
+
+INSERT INTO employee (id , salary , department) VALUE (3 , 200000 , 'HR'),
+(4 , 250000 , 'IT'),
+(5 , 300000 , 'Finance'),
+(6 , 307000 , 'IT'),
+ (7 , 300000 , 'Finance'),
+(8 , 390000 , 'HR'),
+(9 , 300500 , 'Finance');
+
+ SELECT count(employee.name) ,employee.department FROM employee GROUP BY(employee.department);
+SELECT employee.name FROM employee WHERE name LIKE 'A%';
+SELECT * FROM employee ORDER BY salary DESC ;
+SELECT (SELECT DISTINCT salary FROM employee ORDER BY salary DESC LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+UPDATE employee SET department = 'IT' WHERE id =1;
+SELECT e2.name, e1.department FROM employee as e1 INNER JOIN employee as e2;
+SELECT name, MIN(department) AS department FROM employee GROUP BY name;
 
 CREATE TABLE teacher (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -76,3 +101,24 @@ CREATE TABLE teacher2 (
 
 SELECT * FROM teacher2;
 
+CREATE TABLE customer (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    item VARCHAR(55) NOT NULL,
+    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+INSERT INTO customer VALUE(1,'Ishwari'),
+    (2, 'Bhakti'),
+    (3 , 'Keshav');
+
+INSERT INTO orders VALUE (1 , 1 , 'Milk'),
+    (2 , 2 , 'Cookies'),
+    (3 , 2 , 'Makhan');
+
+SELECT c.name FROM customer as c LEFT JOIN orders as o ON c.id = o.customer_id WHERE o.customer_id IS NULL;
